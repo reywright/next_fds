306a2c1b385034976ddf51a62a0efcceb9dd6f6b
Rey Wright <wright.rey@gmail.com>
2021-03-04 19:35:09 -0600

    Dialog and Toaster now work, with the aid of next dynamic

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Button } from '@cbinsights/fds/lib/components'
+import React, { useState } from "react";
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import styles from "../styles/Home.module.css";
+import { Button } from "@cbinsights/fds/lib/components";
+
+const Dialog = dynamic(() => import("@cbinsights/fds/lib/components/Dialog"), {
+  ssr: false,
+});
+
+const Toaster = dynamic(
+  () => import("@cbinsights/fds/lib/components/Toaster"),
+  { ssr: false }
+);
 
 export default function Home(props) {
-  console.log(props);
+  const [isOpen, setIsOpen] = useState(false);
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+
+  const [isToastShowing, setIsToastShowing] = useState(false);
   return (
     <div className={styles.container}>
       <Head>
@@ -17,9 +32,40 @@ export default function Home(props) {
         </h1>
 
         <div className="margin--top--l">
-          <Button label="Hello" />
+          <div className="margin--bottom">
+            <Button label="Trigger Dialog" onClick={() => open()} />
+          </div>
+          <Dialog
+            isOpen={isOpen}
+            content={
+              <>
+                <p className="margin--bottom">
+                  My job is done here, please close me.
+                </p>
+                <Button label="Ok if you insist." onClick={() => close()} />
+              </>
+            }
+            onDismiss={close}
+            title="Dialog works!"
+          />
+          <div>
+            <Button
+              onClick={() => setIsToastShowing(!isToastShowing)}
+              label={isToastShowing ? "Hide Toaster" : "Show Toaster"}
+            />
+          </div>
+          <Toaster
+            isOpen={isToastShowing}
+            toastInstance={{
+              content: "hey",
+              onDismiss: () => setIsToastShowing(false),
+              type: "info",
+              progress: 42,
+              dismissOnAction: true,
+              actionLabel: "Action",
+            }}
+          />
         </div>
-
       </main>
 
       <footer className={styles.footer}>
@@ -28,18 +74,10 @@ export default function Home(props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
-}
-
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      ok: 'foo'
-    }, // will be passed to the page component as props
-  }
+  );
 }
